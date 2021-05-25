d6ac2cb5727a2a9db5b16ec40eec64f54ef10538
Michael Cabana <m.cabana@ieg-america.com>
2021-05-25 09:58:28 -0400

    fixed email magic link auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,56 +4,56 @@ import NextAuth, { InitOptions } from 'next-auth';
 import Providers from 'next-auth/providers';
 
 const options: InitOptions = {
-    // Configure one or more authentication providers
-    secret: process.env.APP_SECRET,
+	// Configure one or more authentication providers
+	secret: process.env.APP_SECRET,
 
-    // pages: {
-    //     signIn: '/auth/signin'
-    // },
-   
-    providers: [
-        // Providers.Email({
-        // 	server: {
-        // 		host: process.env.EMAIL_SERVER_HOST,
-        // 		port: parseInt(process.env.EMAIL_SERVER_PORT),
-        // 		auth: {
-        // 			user: process.env.EMAIL_SERVER_USER,
-        // 			pass: process.env.EMAIL_SERVER_PASSWORD,
-        // 		},
-        // 	},
-        // 	from: process.env.EMAIL_FROM,
-        // }),
-        // Providers.IdentityServer4({
-        // 	id: 'demo-identityserver-io',
-        // 	name: 'demo.identityserver.io',
-        // 	scope: 'openid profile email api offline_access',
-        // 	domain: 'demo.identityserver.io',
-        // 	clientId: 'interactive.public',
-        // 	protection: 'pkce',
-        // }),
-        Providers.Google({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
-        }),
-        // ...add more providers here
-    ],
+	// pages: {
+	//     signIn: '/auth/signin'
+	// },
 
-    // session: {
-    // 	jwt: true,
-    // },
+	providers: [
+		Providers.Email({
+			server: {
+				host: process.env.EMAIL_SERVER_HOST,
+				port: parseInt(process.env.EMAIL_SERVER_PORT),
+				auth: {
+					user: process.env.EMAIL_SERVER_USER,
+					pass: process.env.EMAIL_SERVER_PASSWORD,
+				},
+			},
+			from: process.env.EMAIL_FROM,
+		}),
+		// Providers.IdentityServer4({
+		// 	id: 'demo-identityserver-io',
+		// 	name: 'demo.identityserver.io',
+		// 	scope: 'openid profile email api offline_access',
+		// 	domain: 'demo.identityserver.io',
+		// 	clientId: 'interactive.public',
+		// 	protection: 'pkce',
+		// }),
+		Providers.Google({
+			clientId: process.env.GOOGLE_ID,
+			clientSecret: process.env.GOOGLE_SECRET,
+		}),
+		// ...add more providers here
+	],
 
-    // A database is optional, but required to persist accounts in a database
-    // database: {
-    // 	type: 'postgres',
-    // 	host: process.env.DB_HOST,
-    // 	port: parseInt(process.env.DB_PORT),
-    // 	username: process.env.DB_USER,
-    // 	password: process.env.DB_PASS,
-    // 	database: process.env.DB,
-    // 	synchronize: true,
-    // },
+	// session: {
+	// 	jwt: true,
+	// },
 
-    // debug: true,
+	// A database is optional, but required to persist accounts in a database
+	database: {
+		type: 'postgres',
+		host: process.env.DB_HOST,
+		port: parseInt(process.env.DB_PORT),
+		username: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		database: process.env.DB,
+		synchronize: true,
+	},
+
+	// debug: true,
 };
 
 export default (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, options);
